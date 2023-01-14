8083db2cbb136b29c289d4c6c77e66e7a00abdab
Andrew MacIsaac <amacisaac95@gmail.com>
2023-01-13 20:28:43 -0700

    commit tree 1976756e665126eb7520ac79aec76e2bcbe8100b

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(value) {
+    let blocks;
+    if (value >= 42){
+        blocks = value - 42;
+    }
+    else if ( value < 42 ){
+        blocks = 42 - value;
+    }
+    return blocks;
+}
+function distanceFromHqInFeet(value){
+   let feet = distanceFromHqInBlocks(value) * 264;
+   return feet;
+
+}
+function distanceTravelledInFeet(value1, value2){
+    let feet;
+    if (value1 >= value2){
+        feet = (value1 - value2) * 264;
+    } 
+    else if (value1 < value2){
+        feet = (value2 - value1) * 264;
+    }
+    return feet;
+
+}
+function calculatesFarePrice(value1, value2){
+    let fare;
+    let message;
+    let feet = distanceTravelledInFeet(value1 , value2)
+    if (feet <= 400){
+        message = 0;
+    } else if (400 <= feet && feet <= 2000){
+        fare = (feet * .02) - 8;
+        message = fare;
+    } else if (feet > 2000 && feet <= 2500){
+        message = 25;
+    } else if (feet > 2500){
+        message = "cannot travel that far"
+    }
+    return message;
+
+}
